72c78f4eaebd76b2b9a08c889c49268ae4091b16
rayhans358 <rayhans358@gmail.com>
2023-12-02 00:31:11 +0700

    Finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
+import { useEffect } from 'react';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
-import Navbar from './components/navbarSection';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { listen } from './app/listener';
+import Home from './pages/Home';
+import Account from './pages/Account';
+import Cart from './pages/Cart';
+import Invoice from './pages/Invoice';
+// import Navbar from './components/navbarSection';
 // import Footer from './components/footerSection';
 
 function App() {
+  // Call Listener from features
+  useEffect(() => {
+    listen()
+  }, []);
+  
   return (
     <div>
       <BrowserRouter>
-        <Navbar/>
-        {/* <Footer/> */}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/account' element={<Account/>}/>
+        <Route path='/checkout' element={<Cart/>}/>
+        <Route path='/invoice/:order_id' element={<Invoice/>}/>
+        <Route path='/*' element={<Navigate replace to='/'/>}/>
+      </Routes>
       </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
