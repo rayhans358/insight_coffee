5dfd763fa44598ce11b56ed26cb9cdcccd6c1532
rayhans358 <rayhans358@gmail.com>
2024-05-11 23:11:57 +0700

    Add Style SASS

--- src/config.js
+++ src/config.js
@@ -11,19 +11,7 @@ const config = {
 	owner: process.env.REACT_APP_OWNER,
 	contact: process.env.REACT_APP_CONTACT,
 	billing: {
-		account_no: process.env.REACT_APP_BILLING_NO,
-		bank_name1: process.env.REACT_APP_BILLING_BANK1,
-		bank_name2: process.env.REACT_APP_BILLING_BANK2,
-		bank_name3: process.env.REACT_APP_BILLING_BANK3,
-		bank_name4: process.env.REACT_APP_BILLING_BANK4,
-		bank_name5: process.env.REACT_APP_BILLING_BANK5,
-		bank_name6: process.env.REACT_APP_BILLING_BANK6,
-		bank_name7: process.env.REACT_APP_BILLING_BANK7,
-		bank_name8: process.env.REACT_APP_BILLING_BANK8,
-		bank_name9: process.env.REACT_APP_BILLING_BANK9,
-		bank_name10: process.env.REACT_APP_BILLING_BANK10,
-		another_transfer1: process.env.REACT_APP_BILLING_ANOTHER1,
-		another_transfer2: process.env.REACT_APP_BILLING_ANOTHER2
+		account_no: process.env.REACT_APP_BILLING_NO
 	}
 };
 
